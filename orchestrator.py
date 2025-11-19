929276f61f5386023619b83f5245b3186e1d762d
collinvt <collinvt@uci.edu>
2025-11-19 15:59:04 -0800

    Add file header to orchestrator.py
    Added file header with authors and description.

--- orchestrator.py
+++ orchestrator.py
@@ -1,3 +1,13 @@
+"""
+File: app.py
+Authors: 
+    - [Zachery Thomas] ([47642149])
+    - [Collin Vinh Tran] ([47304556])
+    - [Jenny Thao Ly] ([83605957])
+    - [Lina Nguyen] ([70703520])
+Description: [What this file does (functions/methods used)]
+"""
+
 # Coordinates the multi-agent workflow:
 #   1. PlannerAgent -> produce plan.
 #   2. CoderAgent   -> generate application code.
