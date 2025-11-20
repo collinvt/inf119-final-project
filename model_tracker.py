6a34bcf1d2c1def8e946320937dc801cfae6a59b
collinvt <collinvt@uci.edu>
2025-11-19 16:00:11 -0800

    Add header documentation to model_tracker.py
    Added file header with authors and description.

--- model_tracker.py
+++ model_tracker.py
@@ -1,3 +1,13 @@
+"""
+File: model_tracker.py
+Authors: 
+    - [Zachery Thomas] ([47642149])
+    - [Collin Vinh Tran] ([47304556])
+    - [Jenny Thao Ly] ([83605957])
+    - [Lina Nguyen] ([70703520])
+Description: [What this file does (functions/methods used)]
+"""
+
 # Defines a utility class that tracks how often each
 # model is called and how many tokens are used in total per model.
 
