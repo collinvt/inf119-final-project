64eeb9e3cb22649cdf205893b30eaeb88224e75d
collinvt <collinvt@uci.edu>
2025-11-19 16:00:39 -0800

    Add authors and description to logging_config.py
    Added authors and description to the logging_config.py file.

--- logging_config.py
+++ logging_config.py
@@ -1,3 +1,13 @@
+"""
+File: logging_config.py
+Authors: 
+    - [Zachery Thomas] ([47642149])
+    - [Collin Vinh Tran] ([47304556])
+    - [Jenny Thao Ly] ([83605957])
+    - [Lina Nguyen] ([70703520])
+Description: [What this file does (functions/methods used)]
+"""
+
 # Module provides consistent logging setup across all components
 
 import logging
