11c2d69f51686e87508cda3f1a2dd8f0bf44b016
collinvt <collinvt@uci.edu>
2025-11-19 16:01:03 -0800

    Add header comments to utils.py
    Add file header with authors and description.

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+"""
+File: utils.py
+Authors: 
+    - [Zachery Thomas] ([47642149])
+    - [Collin Vinh Tran] ([47304556])
+    - [Jenny Thao Ly] ([83605957])
+    - [Lina Nguyen] ([70703520])
+Description: [What this file does (functions/methods used)]
+"""
 
 import re
 
